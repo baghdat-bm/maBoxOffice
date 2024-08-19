5a9802276d7d725d89ae3ffc3e8fddcd8942ab41
baghdat-bm <baghdat.bm@gmail.com>
2024-08-19 05:00:02 +0500

    модальное окно сделал чуть шире

--- webapp/ticket_sales/utils.py
+++ webapp/ticket_sales/utils.py
@@ -3,12 +3,14 @@ from django.db import models
 
 
 def update_ticket_amount(ticket_sale):
-    total_amount = TicketSalesService.objects.filter(ticket_sale=ticket_sale).aggregate(total=models.Sum('total_amount'))['total'] or 0
+    total_amount = TicketSalesService.objects.filter(
+        ticket_sale=ticket_sale).aggregate(total=models.Sum('total_amount'))['total'] or 0
     ticket_sale.amount = total_amount
     ticket_sale.save()
 
 
 def update_ticket_paid_amount(ticket_sale):
-    total_amount = TicketSalesPayments.objects.filter(ticket_sale=ticket_sale).aggregate(total=models.Sum('amount'))['total'] or 0
+    total_amount = TicketSalesPayments.objects.filter(
+        ticket_sale=ticket_sale).aggregate(total=models.Sum('amount'))['total'] or 0
     ticket_sale.paid_amount = total_amount
     ticket_sale.save()
